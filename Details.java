68d97c2a7066ba8b83de286aa4cab39618a3deb7
EC2 Default User <ec2-user@ip-172-31-15-18.ap-south-1.compute.internal>
2024-02-25 09:31:12 +0000

    gitstash Command practise

--- Details.java
+++ Details.java
@@ -5,3 +5,9 @@ Do have another method(){
 	it is more complecated works to main
         it is very fully accupied and maintaince
 `}
+
+git stash(){
+
+	it will write more than it will cover
+		to make it very easy and Main Restore Them
+}
